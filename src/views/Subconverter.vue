7c591f316e151a993577fdd2fdc82141da44d1fe
Ash <61870703+shshbb@users.noreply.github.com>
2026-05-24 17:49:16 +0800

    Update backend URL to use secure HTTPS

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -240,7 +240,7 @@ export default {
       // 配置选项
       options: {
         clientTypes: CLIENT_TYPES,
-        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [{ value: "https://subapi.shcyblog.com/sub?" }],
         remoteConfig: REMOTE_CONFIGS
       },
 
